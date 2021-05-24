13991d4535edee1277d94ead18785a570b0ffcd8
bhushan-borole <borolebhushan8@gmail.com>
2021-05-24 12:33:32 +0530

    Update build_readme to auto update categorised readme

--- build_readme.py
+++ build_readme.py
@@ -24,26 +24,52 @@ totals = {
     'Bit Manipulation': 10
 }
 
-for folder in os.listdir(ALL_SOLUTIONS):
-    count[folder] = len(os.listdir(str(ALL_SOLUTIONS) + '/' + folder))
+def update_root_readme(path):
+    for folder in os.listdir(ALL_SOLUTIONS):
+        count[folder] = len(os.listdir(str(ALL_SOLUTIONS) + '/' + folder)) - 1
 
-now = datetime.datetime.now().date()
-op = ''
+    now = datetime.datetime.now().date()
+    op = ''
 
-for key in totals.keys():
-    if key in count:
-        op += f'{key}: {count[key]}/{totals[key]}\n'
+    for key in totals.keys():
+        if key in count:
+            op += f'{key}: {count[key]}/{totals[key]}\n'
 
-final_output = f'''
-# 450 DSA
+    final_output = f'''
+    # 450 DSA
 
-{op}
+    {op}
 
-Last Update on {now}.
-'''
+    Last Update on {now}.
+    '''
 
 
-with open('README.md', 'w+') as f:
-    f.truncate(0)
-    f.write(final_output)
+    with open('README.md', 'w+') as f:
+        f.truncate(0)
+        f.write(final_output)
 
+def update_individual_readme(path):
+    for folder in os.listdir(ALL_SOLUTIONS):
+        folder = str(ALL_SOLUTIONS) + '/' + folder
+        if not os.path.isfile(folder):
+            all_files = os.listdir(folder)
+            all_files.remove('README.md')
+
+            if all_files:
+                all_ids = [int(x[:2]) for x in all_files]
+
+                with open(f'{folder}/README.md', 'r') as f:
+                    all_lines = f.readlines()
+                    for idx in all_ids:
+                        print(all_lines[idx + 2])
+                        all_lines[idx + 2] = all_lines[idx + 2].replace('[ ]', '[x]')
+                        print(all_lines[idx + 2])
+                    f.close()
+
+                with open(f'{folder}/README.md', 'w') as f:
+                    new_contents = ''.join(all_lines)
+                    f.write(new_contents)
+                    f.close()
+
+update_individual_readme(ALL_SOLUTIONS)
+update_root_readme(ALL_SOLUTIONS)
---
+++ solutions/Bit Manipulation/README.md
@@ -0,0 +1,12 @@
+# Bit Manipulation
+
+- [ ] [Count set bits in an integer](https://practice.geeksforgeeks.org/problems/set-bits0143/1)
+- [ ] [Find the two non-repeating elements in an array of repeating elements](https://practice.geeksforgeeks.org/problems/finding-the-numbers0215/1)                                      
+- [ ] [Count number of bits to be flipped to convert A to B](https://practice.geeksforgeeks.org/problems/bit-difference/0)                                                                
+- [ ] [Count total set bits in all numbers from 1 to n](https://practice.geeksforgeeks.org/problems/count-total-set-bits/0)                                                               
+- [ ] [Program to find whether a no is power of two](https://practice.geeksforgeeks.org/problems/power-of-2/0)                                                                            
+- [ ] [Find position of the only set bit](https://practice.geeksforgeeks.org/problems/find-position-of-set-bit3706/1)                                                                     
+- [ ] [Copy set bits in a range](https://www.geeksforgeeks.org/copy-set-bits-in-a-range/)                                                                                                 
+- [ ] [Divide two integers without using multiplication, division and mod operator](https://www.geeksforgeeks.org/divide-two-integers-without-using-multiplication-division-mod-operator/)
+- [ ] [Calculate square of a number without using \*, / and pow()](https://www.geeksforgeeks.org/calculate-square-of-a-number-without-using-and-pow/)                                     
+- [ ] [Power Set](https://practice.geeksforgeeks.org/problems/power-set4302/1)                                                                                                            
