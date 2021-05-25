35da14ef89fd867597a3c83f2033e114142c7d23
bhushan-borole <borolebhushan8@gmail.com>
2021-05-25 22:13:54 +0530

    Add longest_palindrome_substring

---
+++ solutions/String/07_longest_palindrome_substring.py
@@ -0,0 +1,22 @@
+def longestPalindrome(s):
+    if not s or len(s) < 1:
+        return ''
+    
+    start, end = 0, 0
+    
+    for i in range(len(s)):
+        len1 = helper(s, i, i) # even palindrome
+        len2 = helper(s, i, i+1) # odd palindrome
+        max_len = max(len1, len2)
+        if max_len > end - start:
+            start = i - (max_len - 1) // 2
+            end = i + max_len // 2
+        
+    return s[start: end + 1]
+
+def helper(s, left, right):
+    while(left >= 0 and right < len(s) and s[left] == s[right]):
+        left -= 1
+        right += 1
+    
+    return right - left - 1
