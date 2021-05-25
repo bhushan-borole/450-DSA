1c3cf5e92f6b8eaef559f416031f624700266ba1
bhushan-borole <borolebhushan8@gmail.com>
2021-05-25 09:33:58 +0530

    Add reverse_a_string

---
+++ solutions/String/00_reverse_a_string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse_string(self, s):
+        # s : List[str]
+        s.reverse()
+
+# Two pointer approach
+class Solution:
+    def reverse_string(self, s):
+        # s : List[str]
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
