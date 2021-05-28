a31bc5fce8235731fea4617a61c3ca3ba8692c82
bhushan-borole <borolebhushan8@gmail.com>
2021-05-28 08:23:06 +0530

    Add function call

--- solutions/Matrix/01_search_in_matrix.py
+++ solutions/Matrix/01_search_in_matrix.py
@@ -3,3 +3,11 @@ def searchMatrix(matrix, target):
         if target in row:
             return True
     return False
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [0, 1, 0, 3],
+    [7, 4, 5, 6]
+]
+print(searchMatrix(matrix, 7))
